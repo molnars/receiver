6d3a7353ce305ab0751f2817074f14adbabaa994
molnars <szabolcs.molnar@gmail.com>
2022-06-07 17:25:00 +0400

    emailer

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 'use strict';
 
 const express = require('express');
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const env = require('env-var');
 
 // Constants
-const PORT = 8080;
+const PORT = env.get('LISTENPORT').default('8080');
 const HOST = '0.0.0.0';
+const SMTPHOST = env.get('SMTPHOST').required();
+const SMTPPORT = env.get('SMTPPORT').default('25').asPortNumber();
+const SMTPSECURE = env.get('SMTPSECURE').default('false');
+const SMTPFROM = env.get('SMTPFROM').required();
+const SMTPTO = env.get('SMTPTO').required();
+
+
+async function mailer() {
+let transporter = nodemailer.createTransport({
+  host: ${SMTPHOST},
+  port: ${SMTPPORT},
+  secure: ${SMTPSECURE}
+});
+
+let info = await transporter.sendMail({
+  from: '${SMTPFROM}', // sender address
+  to: '${SMTPTO}', // list of receivers
+  subject: "Hello ✔", // Subject line
+  text: "Hello world?", // plain text body
+  html: "<b>Hello world?</b>", // html body
+});
+
+
+console.log("Message sent: %s", info.messageId);
+}
 
 // App
 const app = express();
@@ -19,10 +46,13 @@ app.post("/hook", (req, res) => {
   res.status(200).end() // Responding is important
 })
 
-
 //app.get('/', (req, res) => {
 //  res.send('Hello World');
 //});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`SMTP host:${SMTPHOST}:${SMTPPORT}`);
+//
+//curl -kv http://localhost:8080/hook -XPOST -H "Content-Type: application/json" -d '{"data": "value"}'
+//
