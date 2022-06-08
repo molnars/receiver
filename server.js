14c1343968c8fdd9719a54a0e5f44c8e26fa1171
molnars <szabolcs.molnar@gmail.com>
2022-06-08 17:36:15 +0400

    mailer

--- server.js
+++ server.js
@@ -2,57 +2,55 @@
 
 const express = require('express');
 const bodyParser = require("body-parser");
-//const nodemailer = require("nodemailer");
-//const env = require('env-var');
+const nodemailer = require("nodemailer");
+const env = require('env-var');
+const { detectExtension } = require('nodemailer/lib/mime-funcs/mime-types');
+require('log-timestamp');
 
 // Constants
-//const PORT = env.get('LISTENPORT').default('8080');
-const PORT = '8080';
+const PORT = env.get('LISTENPORT').default('8080').asPortNumber();
+//const PORT = '8080';
 const HOST = '0.0.0.0';
-//const SMTPHOST = env.get('SMTPHOST').required();
-//const SMTPPORT = env.get('SMTPPORT').default('25').asPortNumber();
-//const SMTPSECURE = env.get('SMTPSECURE').default('false');
-//const SMTPFROM = env.get('SMTPFROM').required();
-//const SMTPTO = env.get('SMTPTO').required();
+const SMTPHOST = env.get('SMTPHOST').default('localhost').required().asString();
+const SMTPPORT = env.get('SMTPPORT').default('25').asPortNumber();
+const SMTPSECURE = env.get('SMTPSECURE').default('false').asBool();
+const SMTPFROM = env.get('SMTPFROM').default('noone@example.net').required().asString();
+const SMTPTO = env.get('SMTPTO').required().asString();
 
 
-//async function mailer() {
-//let transporter = nodemailer.createTransport({
-//  host: SMTPHOST,
-//  port: SMTPPORT,
-//  secure: SMTPSECURE
-//});
+async function mailer() {
+//  return;
+let transporter = nodemailer.createTransport({
+  host: SMTPHOST,
+  port: SMTPPORT,
+  secure: SMTPSECURE
+});
 
-//let info = await transporter.sendMail({
-//  from: '${SMTPFROM}', // sender address
-//  to: '${SMTPTO}', // list of receivers
-//  subject: "Hello ✔", // Subject line
-//  text: "Hello world?", // plain text body
-//  html: "<b>Hello world?</b>", // html body
-//});
+let info = await transporter.sendMail({
+  from: '${SMTPFROM}', // sender address
+  to: '${SMTPTO}', // list of receivers
+  subject: "Hello ✔", // Subject line
+  text: "", // plain text body
+  html: "<b>Hello world?</b>", // html body
+});
 
-
-//console.log("Message sent: %s", info.messageId);
-//}
+console.log("Message sent: %s", info.messageId);
+}
 
 // App
 const app = express();
 app.use(bodyParser.json())
-const dateObject = new Date();
-const date = (`0${dateObject.getDate()}`).slice(-2);
-const month = (`0${dateObject.getMonth() + 1}`).slice(-2);
-const year = dateObject.getFullYear();
-const hours = dateObject.getHours();
-const minutes = dateObject.getMinutes();
-const seconds = dateObject.getSeconds();
-
 app.get("/", (req, res) => {
   console.log("get")
   res.status(200).end()
 })
 app.post("/hook", (req, res) => {
-  console.log("".concat(`${year}-${month}-${date} ${hours}:${minutes}:${seconds}: `).concat(JSON.stringify(req.body))) // Call your action on the request here
-  res.status(200).end() // Responding is important
+  console.log(req.body) // Call your action on the request here
+  mailer().catch((err) => {
+    console.error("Send mail error:", err.message)
+    res.status(500).end();
+  })
+  res.status(200).end();
 })
 
 //app.get('/', (req, res) => {
@@ -61,7 +59,7 @@ app.post("/hook", (req, res) => {
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
-//console.log(`SMTP host:${SMTPHOST}:${SMTPPORT}`);
+console.log(`SMTP host:${SMTPHOST}:${SMTPPORT}`);
 //
 //curl -kv http://localhost:8080/hook -XPOST -H "Content-Type: application/json" -d '{"data": "value"}'
 //
